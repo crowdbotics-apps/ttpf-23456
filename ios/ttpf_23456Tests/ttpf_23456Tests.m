377fe6cd823305c2087d439f54c18cfbf278aae3
Crowdbotics <team@crowdbotics.com>
2020-12-19 01:12:20 +0000

    Initial commit.

---
+++ ios/ttpf_23456Tests/ttpf_23456Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface ttpf_23456 : XCTestCase
+
+@end
+
+@implementation ttpf_23456
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
